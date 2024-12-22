9aa5ea44e2a3758ba8daffd36eb02aab14e66ee3
Aniol Lopez Ortega <99729558+Aniolobolo@users.noreply.github.com>
2024-12-22 12:53:19 +0100

    Creacion de pantalla inicial y de menú

--- RacingGameFisica2/Source/Main.cpp
+++ RacingGameFisica2/Source/Main.cpp
@@ -1,87 +1,156 @@
 #include "Application.h"
 #include "Globals.h"
-
+#include "ModuleRender.h"
 #include "raylib.h"
 
 #include <stdlib.h>
 
 enum main_states
 {
-	MAIN_CREATION,
-	MAIN_START,
-	MAIN_UPDATE,
-	MAIN_FINISH,
-	MAIN_EXIT
+    MAIN_CREATION,
+    MAIN_START,
+    MAIN_UPDATE,
+    MAIN_FINISH,
+    MAIN_EXIT,
+    MAIN_SCREEN1,
+    MAIN_SCREEN2
 };
 
-int main(int argc, char ** argv)
+Texture2D screen1Image;
+Texture2D screen2Image;
+
+void DrawScreen1()
 {
-	LOG("Starting game '%s'...", TITLE);
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    DrawTexture(screen1Image, 940,860, WHITE);
+    DrawText("Pantalla 1: Presiona SPACE para continuar", 190, 200, 20, RED);
+    EndDrawing();
+}
 
-	int main_return = EXIT_FAILURE;
-	main_states state = MAIN_CREATION;
-	Application* App = NULL;
+void DrawScreen2()
+{
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    DrawTexture(screen2Image, 880, 840, WHITE);
+    DrawText("Pantalla 2: Presiona T para continuar", 190, 200, 20, RED);
+    EndDrawing();
+}
 
-	while (state != MAIN_EXIT)
-	{
-		switch (state)
-		{
-		case MAIN_CREATION:
+int main(int argc, char** argv)
+{
+    LOG("Starting game '%s'...", TITLE);
 
-			LOG("-------------- Application Creation --------------");
-			App = new Application();
-			state = MAIN_START;
-			break;
+    int main_return = EXIT_FAILURE;
+    main_states state = MAIN_CREATION;
+    Application* App = NULL;
 
-		case MAIN_START:
+    InitWindow(800, 600, TITLE); // Inicializar la ventana
 
-			LOG("-------------- Application Init --------------");
-			if (App->Init() == false)
-			{
-				LOG("Application Init exits with ERROR");
-				state = MAIN_EXIT;
-			}
-			else
-			{
-				state = MAIN_UPDATE;
-				LOG("-------------- Application Update --------------");
-			}
+    // Cargar las imágenes
+    screen1Image = LoadTexture("Assets/mainScreen.png");
+    if (screen1Image.id == 0)
+    {
+        LOG("Error al cargar la imagen Assets/crate.png");
+    }
+    else
+    {
+        LOG("Imagen Assets/crate.png cargada correctamente");
+    }
 
-			break;
+    screen2Image = LoadTexture("Assets/holaScreen.png");
+    if (screen2Image.id == 0)
+    {
+        LOG("Error al cargar la imagen Assets/holaScreen.png");
+    }
+    else
+    {
+        LOG("Imagen Assets/holaScreen.png cargada correctamente");
+    }
 
-		case MAIN_UPDATE:
-		{
-			int update_return = App->Update();
+    while (state != MAIN_EXIT && !WindowShouldClose())
+    {
+        switch (state)
+        {
+        case MAIN_CREATION:
 
-			if (update_return == UPDATE_ERROR)
-			{
-				LOG("Application Update exits with ERROR");
-				state = MAIN_EXIT;
-			}
+            LOG("-------------- Application Creation --------------");
+            App = new Application();
+            state = MAIN_START;
+            break;
 
-			if (update_return == UPDATE_STOP)
-				state = MAIN_FINISH;
-		}
-			break;
+        case MAIN_START:
 
-		case MAIN_FINISH:
+            LOG("-------------- Application Init --------------");
+            if (App->Init() == false)
+            {
+                LOG("Application Init exits with ERROR");
+                state = MAIN_EXIT;
+            }
+            else
+            {
+                state = MAIN_SCREEN1;
+                LOG("-------------- Application Screen 1 --------------");
+            }
 
-			LOG("-------------- Application CleanUp --------------");
-			if (App->CleanUp() == false)
-			{
-				LOG("Application CleanUp exits with ERROR");
-			}
-			else
-				main_return = EXIT_SUCCESS;
+            break;
 
-			state = MAIN_EXIT;
+        case MAIN_SCREEN1:
+            DrawScreen1();
+            if (IsKeyPressed(KEY_SPACE))
+            {
+                state = MAIN_SCREEN2;
+                LOG("-------------- Application Screen 2 --------------");
+            }
+            break;
 
-			break;
+        case MAIN_SCREEN2:
+            DrawScreen2();
+            if (IsKeyPressed(KEY_T))
+            {
+                state = MAIN_UPDATE;
+                LOG("-------------- Application Update --------------");
+            }
+            break;
 
-		}
-	}
+        case MAIN_UPDATE:
+        {
+            int update_return = App->Update();
 
-	delete App;
-	LOG("Exiting game '%s'...\n", TITLE);
-	return main_return;
+            if (update_return == UPDATE_ERROR)
+            {
+                LOG("Application Update exits with ERROR");
+                state = MAIN_EXIT;
+            }
+
+            if (update_return == UPDATE_STOP)
+                state = MAIN_FINISH;
+        }
+        break;
+
+        case MAIN_FINISH:
+
+            LOG("-------------- Application CleanUp --------------");
+            if (App->CleanUp() == false)
+            {
+                LOG("Application CleanUp exits with ERROR");
+            }
+            else
+                main_return = EXIT_SUCCESS;
+
+            state = MAIN_EXIT;
+
+            break;
+
+        }
+    }
+
+    // Descargar las imágenes
+    UnloadTexture(screen1Image);
+    UnloadTexture(screen2Image);
+
+    delete App;
+    CloseWindow(); // Cerrar la ventana
+    LOG("Exiting game '%s'...\n", TITLE);
+    return main_return;
 }
